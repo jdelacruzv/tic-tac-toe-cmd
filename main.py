d21ff8958b478e5c1b5782b4aa7de5f826c16a17
Jose De La Cruz <jldlcv@gmail.com>
2024-12-08 19:19:39 +0100

    added display_board function

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
+from random import randrange
+
+
+def display_board(board):
+	print("+-------" * 3, "+", sep="")
+	for row in range(3):
+		print("|       " * 3, "|", sep="")
+		for col in range(3):
+			print("|   " + str(board[row][col]) + "   ", end="")
+		print("|")
+		print("|       " * 3, "|", sep="")
+		print("+-------" * 3, "+", sep="")
+
+
 def welcome_title():
 	print("\nWelcome to Tic-Tac-Toe!\n")
 	print("-------" * 2)
@@ -5,5 +20,11 @@ def welcome_title():
 	print("-------" * 2)
 
 
+def main():
+	board = [[3 * j + i + 1 for i in range(3)] for j in range(3)]
+	board[1][1] = "X"
+
+
 if __name__ == "__main__":
 	welcome_title()
+	main()
