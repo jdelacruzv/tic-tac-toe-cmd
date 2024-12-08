d7bbc62541244df99444acc317b19f0e6de0b9d2
Jose De La Cruz <jldlcv@gmail.com>
2024-12-08 18:51:33 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def welcome_title():
+	print("\nWelcome to Tic-Tac-Toe!\n")
+	print("-------" * 2)
+	print("Machine\t: 'X'\nUser\t: 'O'\nSalir\t: 'S'")
+	print("-------" * 2)
+
+
+if __name__ == "__main__":
+	welcome_title()
