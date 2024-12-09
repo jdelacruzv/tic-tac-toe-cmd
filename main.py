427e0a442f4be956be0159e0ca10eb87d98b0075
Jose De La Cruz <jldlcv@gmail.com>
2024-12-09 22:26:25 +0100

    added enter_move_user function

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ def display_board(board):
 		print("+-------" * 3, "+", sep="")
 
 
+def enter_move_user(board):
+	ok = False
+	while not ok:
+		move = input("Ingresa tu movimiento: ")
+		if move.lower() == "s":
+			print("Salio del juego!")
+			os._exit(0)
+		ok = len(move) == 1 and move >= "1" and move <= "9"
+		if not ok:
+			print("Movimiento erróneo, ingrésalo nuevamente")
+			continue
+		move = int(move) - 1
+		row = move // 3
+		col = move % 3
+		num_sel = board[row][col]
+		ok = num_sel not in ['O', 'X']
+		if not ok:
+			print("¡Cuadro ocupado, ingresa nuevamente!")
+			continue
+	board[row][col] = "O"
+
+
 def welcome_title():
 	print("\nWelcome to Tic-Tac-Toe!\n")
 	print("-------" * 2)
